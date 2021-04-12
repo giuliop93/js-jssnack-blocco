de1a8845d6b7060ee1cf7d1dfd65b408bb51a8a9
Giulio_Pa <giuliop93@hotmail.it>
2021-04-12 12:18:25 +0200

    wip

---
+++ es-3/script.js
@@ -0,0 +1,15 @@
+// chiedo all'utente di inserire un numero
+
+
+var numeroUno = prompt("inserisci un numero");
+var numeroDue = prompt("inserisci un numero");
+var numeroTre = prompt("inserisci un numero");
+var numeroQuattro = prompt("inserisci un numero");
+var numeroCinque = prompt("inserisci un numero");
+var numeroSei = prompt("inserisci un numero");
+var numeroSette = prompt("inserisci un numero");
+var numeroOtto = prompt("inserisci un numero");
+var numeroNove = prompt("inserisci un numero");
+var numeroDieci = prompt("inserisci un numero");
+
+for ()
